c7096480197af082ad3b715bce93c0fdca47b14b
hanxianlong <hanxianlong@vancl.com>
2013-04-08 11:31:56 +0800

    modify home data and style

--- wp-content/themes/epachina/sidebar-home.php
+++ wp-content/themes/epachina/sidebar-home.php
@@ -27,22 +27,22 @@ if ( !defined('ABSPATH')) exit;
             <div class="post-entry bottom-dotted-line">
                 <div class="featured-img"><img src="http://hanxianlong.tk/a.jpg" height="150px" width="150px"/></div>
                 <div class="post-content">
-                    <h1 class="post-title"><a href="/">文章标题文章标题文章标题</a></h1>
+                    <h1 class="post-title"><a href="/">全美IPO路演大师Weissman访问中国</a></h1>
                     <p><a href="/"><i>文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容</i></a></p>
                 </div>
             </div>
              <div class="post-entry bottom-dotted-line">
                 <div class="featured-img"><img src="http://hanxianlong.tk/a.jpg" height="150px" width="150px"/></div>
                 <div class="post-content">
-                    <h1 class="post-title"><a href="/">文章标题文章标题文章标题</a></h1>
-                    <p><a href="/"><i>文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容</i></a></p>
+                    <h1 class="post-title"><a href="/">三八妇女节职场幸福感系列讲座</a></h1>
+                    <p><a href="/"><i>新年伊始，妇女节临近，JobExcel为您推出了EAP员工关怀——提升职场幸福感系列讲座与互动实践课程，我们邀请商务礼仪、形象设计、妆容修饰、音乐赏析、亲子沟通等领域内的专家及知名讲师，为企业员工提供最人性化的关怀，繁忙的工作之余，别忘放松自己的身心，提升自我品位修养。</i></a></p>
                 </div>
             </div>
              <div class="post-entry bottom-dotted-line">
                 <div class="featured-img"><img src="http://hanxianlong.tk/a.jpg" height="150px" width="150px"/></div>
                 <div class="post-content">
-                    <h1 class="post-title"><a href="/">文章标题文章标题文章标题</a></h1>
-                    <p><a href="/"><i>文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容</i></a></p>
+                    <h1 class="post-title"><a href="/">将成功率提高一倍的10个基本销售技巧</a></h1>
+                    <p><a href="/"><i>想要确保更多的潜在的高品质的顾客成为实际的终身付费的客户吗？以下十件事是你今天就能开始轻松将你的转换率提高一倍的诀窍，很有可能在这个季度，以及以后帮助你进入公司的“销售冠军圈”</i></a></p>
                 </div>
             </div>
             <div class="more"><a href="/">更多详情》》</a></div>
@@ -65,21 +65,21 @@ if ( !defined('ABSPATH')) exit;
     <hr class="line"/>
     <div class="cases">
         <div class="widget-wrapper case-item">
-            <div class="widget-title-home bottom-solid-line"><h3>精彩案例</h3></div>
+            <div class="widget-title-home bottom-solid-line"><h3><a href="#">精彩案例</a></h3></div>
             <div class=""><img src="http://www.astd.org/~/media/Images/Community-Featured-Image/TOUCHMAP.jpg"/></div>
             <div class="case-entry">
-                <a href="/">New Cases Text</a><br/>
-                <a href="/">Cases2</a><br/>
-                <a href="/">Cases2</a><br/>
+                <a href="/">避免工作和生活中十大错误</a><br/>
+                <a href="/">专业：配合，比专业能力重要</a><br/>
+                <a href="/">演示:打动客户的销售演示</a><br/>
             </div>
         </div>
         <div class="widget-wrapper case-item">
-            <div class="widget-title-home bottom-solid-line"><h3>资源精华</h3></div>
+            <div class="widget-title-home bottom-solid-line"><h3><a href="#">资源精华</a></h3></div>
             <div class=""><img src="http://www.astd.org/~/media/Images/Community-Featured-Image/TOUCHMAP.jpg"/></div>
             <div class="case-entry">
-                <a href="/">New Cases Text</a><br/>
-                <a href="/">Cases2</a><br/>
-                <a href="/">Cases2</a><br/>
+                <a href="/">英文写作：商务英文写作</a><br/>
+                <a href="/">商务演讲：魅力商务演讲</a><br/>
+                <a href="/">文化沟通：跨文化沟通</a><br/>
             </div>
         </div>
         <div class="widget-wrapper right-advertise">
@@ -95,8 +95,23 @@ if ( !defined('ABSPATH')) exit;
             <div id="buttonPrev"></div>
             <div id="imagesContainer">
                 <ul id="frameView">
-                 <li><a href="http://item.vancl.com/0191706.html"><img alt="" src="http://i2.vanclimg.com/www/company/13.png"></a></li><li><a href="http://item.vancl.com/0191630.html"><img alt="" src="http://i3.vanclimg.com/www/company/14.png"></a></li><li><a href="http://item.vancl.com/0192069.html"><img alt="" src="http://i5.vanclimg.com/www/company/11.png"></a></li><li><a href="http://item.vancl.com/0192408.html"><img alt="" src="http://i1.vanclimg.com/www/company/12.png "></a></li><li><a href="http://item.vancl.com/0192606.html"><img alt="" src="http://i4.vanclimg.com/www/company/15.png"></a></li><li><a href="http://home.vancl.com/27541-n10-s1.html"><img alt="" src="http://i5.vanclimg.com/www/company/16.png"></a></li><li><a href="http://miook.vancl.com/27535-s1.html"><img alt="" src="http://i1.vanclimg.com/www/company/17.png"></a></li><li><a href="http://item.vancl.com/0191706.html"><img alt="" src="http://i2.vanclimg.com/www/company/13.png"></a></li><li><a href="http://item.vancl.com/0191630.html"><img alt="" src="http://i3.vanclimg.com/www/company/14.png"></a></li><li><a href="http://item.vancl.com/0192069.html"><img alt="" src="http://i5.vanclimg.com/www/company/11.png"></a></li><li><a href="http://item.vancl.com/0192408.html"><img alt="" src="http://i1.vanclimg.com/www/company/12.png "></a></li><li><a href="http://item.vancl.com/0192606.html"><img alt="" src="http://i4.vanclimg.com/www/company/15.png"></a></li><li><a href="http://home.vancl.com/27541-n10-s1.html"><img alt="" src="http://i5.vanclimg.com/www/company/16.png"></a></li><li><a href="http://miook.vancl.com/27535-s1.html"><img alt="" src="http://i1.vanclimg.com/www/company/17.png"></a></li>
-                 <li><a href="http://item.vancl.com/0191706.html"><img alt="" src="http://i2.vanclimg.com/www/company/13.png"></a></li><li><a href="http://item.vancl.com/0191630.html"><img alt="" src="http://i3.vanclimg.com/www/company/14.png"></a></li><li><a href="http://item.vancl.com/0192069.html"><img alt="" src="http://i5.vanclimg.com/www/company/11.png"></a></li><li><a href="http://item.vancl.com/0192408.html"><img alt="" src="http://i1.vanclimg.com/www/company/12.png "></a></li><li><a href="http://item.vancl.com/0192606.html"><img alt="" src="http://i4.vanclimg.com/www/company/15.png"></a></li><li><a href="http://home.vancl.com/27541-n10-s1.html"><img alt="" src="http://i5.vanclimg.com/www/company/16.png"></a></li><li><a href="http://miook.vancl.com/27535-s1.html"><img alt="" src="http://i1.vanclimg.com/www/company/17.png"></a></li><li><a href="http://item.vancl.com/0191706.html"><img alt="" src="http://i2.vanclimg.com/www/company/13.png"></a></li><li><a href="http://item.vancl.com/0191630.html"><img alt="" src="http://i3.vanclimg.com/www/company/14.png"></a></li><li><a href="http://item.vancl.com/0192069.html"><img alt="" src="http://i5.vanclimg.com/www/company/11.png"></a></li><li><a href="http://item.vancl.com/0192408.html"><img alt="" src="http://i1.vanclimg.com/www/company/12.png "></a></li><li><a href="http://item.vancl.com/0192606.html"><img alt="" src="http://i4.vanclimg.com/www/company/15.png"></a></li><li><a href="http://home.vancl.com/27541-n10-s1.html"><img alt="" src="http://i5.vanclimg.com/www/company/16.png"></a></li><li><a href="http://miook.vancl.com/27535-s1.html"><img alt="" src="http://i1.vanclimg.com/www/company/17.png"></a></li>
+                 <li><a href="#"><img alt="" src="http://jobexcel.com/images/clients/moto.jpg"></a></li>
+                 <li><a href="#"><img alt="" src="http://jobexcel.com/images/clients/moto.jpg"></a></li>
+                 <li><a href="#"><img alt="" src="http://jobexcel.com/images/clients/moto.jpg"></a></li>
+                 <li><a href="#"><img alt="" src="http://jobexcel.com/images/clients/moto.jpg"></a></li>
+                 <li><a href="#"><img alt="" src="http://jobexcel.com/images/clients/moto.jpg"></a></li>
+                 <li><a href="#"><img alt="" src="http://jobexcel.com/images/clients/moto.jpg"></a></li>
+                 <li><a href="#"><img alt="" src="http://jobexcel.com/images/clients/moto.jpg"></a></li>
+                 <li><a href="#"><img alt="" src="http://jobexcel.com/images/clients/moto.jpg"></a></li>
+                 <li><a href="#"><img alt="" src="http://jobexcel.com/images/clients/moto.jpg"></a></li>
+                 <li><a href="#"><img alt="" src="http://jobexcel.com/images/clients/moto.jpg"></a></li>
+                 <li><a href="#"><img alt="" src="http://jobexcel.com/images/clients/moto.jpg"></a></li>
+                 <li><a href="#"><img alt="" src="http://jobexcel.com/images/clients/moto.jpg"></a></li>
+                 <li><a href="#"><img alt="" src="http://jobexcel.com/images/clients/moto.jpg"></a></li>
+                 <li><a href="#"><img alt="" src="http://jobexcel.com/images/clients/moto.jpg"></a></li>
+                 <li><a href="#"><img alt="" src="http://jobexcel.com/images/clients/moto.jpg"></a></li>
+                 
+                 
                 </ul> 
              </div>
             <div id="buttonNext"></div>
